bd719a9d4826e2bc85310f5d11549885f0600649
ahmedmelfay <ahmedmelfay@gmail.com>
2022-03-18 19:02:30 +0200

    added active nav link functionality and added logo

--- src/components/Navbar/components/LinkItem.js
+++ src/components/Navbar/components/LinkItem.js
@@ -1,30 +1,13 @@
 import React, { Component } from "react";
-import { Link,matchPath,MemoryRouter,PathMatch} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "../styles/index.module.scss";
 export class LinkItem extends Component {
-
-  
-  constructor(props) {
-    console.log(MemoryRouter('men'))
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-state={
-  active:false
-}
-
-  handleClick(e){
-    this.setState({active:true})
-    e.stopPropagation() 
-  }
-
   render() {
     return (
       <li className={styles.nav__item}>
-        <Link onClick={this.handleClick} className={`${styles.nav__link} ${this.state.active && styles.active}`} to={`/${this.props.link}`}>
+        <NavLink className={({ isActive }) => (isActive ? `${styles.active} ${styles.nav__link}` : styles.nav__link)} to={`/${this.props.link}`}>
           {this.props.link.toUpperCase()}
-        </Link>
+        </NavLink>
       </li>
     );
   }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import LinkItem from "./components/LinkItem";
 import styles from "./styles/index.module.scss";
+import logo from "../../images/logo.svg";
 export class Navbar extends Component {
   render() {
     return (
@@ -12,6 +13,8 @@ export class Navbar extends Component {
               <LinkItem link="men" />
               <LinkItem link="children" />
             </ul>
+            <img className={styles.logo} src={logo} alt="Scandiweb" />
+            <div className={styles.nav__right__side}></div>
           </div>
         </div>
       </nav>
